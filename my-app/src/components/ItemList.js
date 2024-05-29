d6998f975817e5280499b9f7343afa136dc3d57d
DESKTOP-UG2A50B\User <dot0112dot0112@gmail.com>
2024-05-29 14:42:03 +0900

    14:42

--- my-app/src/components/ItemList.js
+++ my-app/src/components/ItemList.js
@@ -2,9 +2,9 @@ import React from "react";
 import '../styles/item.css';
 import ItemBox from "./ItemBox";
 
-function ItemList({ pageNum }) {
-    const startIndex = (pageNum - 1) * 12 + 1; // 페이지마다 시작 인덱스 조정
-    const items = Array.from({ length: 12 }, (_, index) => startIndex + index);
+function ItemList({ pageNum, viewItems = 12 , itemList }) {
+    const startIndex = (pageNum - 1) * viewItems + 1; // 페이지마다 시작 인덱스 조정
+    const items = Array.from({ length: viewItems }, (_, index) => startIndex + index);
 
     return (
         <div className="grid-container" >
---
+++ my-app/src/components/SearchNames.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import '../styles/search.css'
+import ReactModal from "react-modal";
+
+function SearchName({onClose, itemList}){
+    const handleClickCancel = () => {
+        onClose();
+    };
+
+    
+}
